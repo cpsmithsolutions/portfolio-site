258e2b03507951e2dc85a5c04887342061787160
clarkpsmith <clark.smith79@gmail.com>
2023-05-15 16:15:09 -0600

    add OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,38 @@
-import Script from 'next/script'
+'use client'
 import './globals.css'
 import { Roboto } from 'next/font/google'
-
 
 // const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto({
   weight: ['400', '700'],
   subsets: ['latin'],
-});
+})
 
 export const metadata = {
   title: 'Clark Smith Full Stack Developer',
-  description: 'The Portfoli site of Clark Smith, Full Stack Developer',
+  description: 'The Portfolio site of Clark Smith, Full Stack Developer',
+  creator: 'Clark Smith',
+  openGraph: {
+    title: 'Clark Smith Full Stack Developer',
+    description: 'The Portfolio site of Clark Smith, Full Stack Developer',
+    url: 'https://www.clarksmith.dev',
+    siteName: 'Clark Smith Full Stack Developer',
+    images: [
+      {
+        url: '/images/preview.jpg',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: '/images/preview.jpg',
+        width: 1800,
+        height: 1600,
+        alt: 'preview image',
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
