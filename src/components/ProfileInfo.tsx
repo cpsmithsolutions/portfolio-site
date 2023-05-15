fcddf672dc8b9e108c8aead5941049f8a9ee06b6
clarkpsmith <clark.smith79@gmail.com>
2023-05-14 20:35:04 -0600

    first commit

---
+++ src/components/ProfileInfo.tsx
@@ -0,0 +1,66 @@
+
+
+'use client'
+import React from "react"
+import { motion } from "framer-motion"
+import Image from "next/image"
+
+const ProfileInfo = ({data}) => {
+    const profile = data.items[0].fields
+
+return (
+    <div>
+         <motion.div
+        className="absolute left-[130px] top-[60px] sm:left-[160px] sm:top-[78px] lg:left-[200px] lg:top-[100px] xl:left-[235px]"
+         initial={{ opacity: 0, x: '300px' }}
+         animate={{ opacity: 1, x: '0px' }}
+         transition={{ duration: 1.5, delay: 1}}
+        >
+             <h1 className="text-[32px] sm:text-[48px] lg:text-[64px] font-bold ml-4">{profile.name}</h1>
+        </motion.div>
+        <motion.div
+         initial={{ opacity: 0, x: '-300px' }}
+         animate={{ opacity: 1, x: '0px' }}
+         transition={{ duration: 1, delay: 1}}
+        >
+          <div 
+          
+          className="w-[100px] rounded-md border border-[#818589] mb-2 overflow-hidden">
+            <Image
+                src={`https:${profile.imageUrl.fields.file.url}`}
+                alt="Profile Photo"
+                className="dark:invert"
+                width={100}
+                height={100}
+                priority />
+        </div>
+        </motion.div>
+    <motion.div
+    initial={{ opacity: 0, y: '-300px' }}
+    animate={{ opacity: 1, y: '0px' }}
+    transition={{ duration: 1, delay: 0}}
+    >
+        <div className="flex">
+      
+    
+          
+<div
+
+>
+       
+        </div>
+
+    </div><h2 className="mt-4 text-[24px] md:text-[32px] ">{profile.description}</h2><div
+    >
+            <p className="mt-2">{profile.bio}</p>
+        </div>
+        
+        </motion.div>
+       
+        </div>
+
+             
+)
+}
+
+export default ProfileInfo
---
+++ src/components/ProjectComponent.tsx
@@ -0,0 +1,70 @@
+'use client'
+import Link from "next/link"
+import React, {useState, useEffect} from "react"
+import Image from "next/image"
+import { motion, AnimatePresence } from "framer-motion"
+import { useMediaQuery } from 'react-responsive'
+
+const ProjectComponent = ({data}) => {
+    const [showVisitSite, setShowVisitSite] = useState<boolean>(false)
+    const [isMedium, setIsMedium] = useState(true)
+    
+    const isMediumSize = useMediaQuery({
+        query: '(min-width: 640px)'
+      })
+
+useEffect(() => {
+    setIsMedium(isMediumSize)
+}, [isMediumSize])
+
+
+    console.log({data})
+    console.log({isMedium})
+
+    return (
+        <motion.div className="py-4 flex flex-col justify-center items-center"
+      
+        >
+            <Link href={data.projectUrl} target="_blank">
+              <h3 className="text-[24px] md:text-[32px] my-4 text-center transition duration-500 ease-in-out ease-in-out hover:text-[#6082B6]" >
+             {data.title}
+              </h3>
+              </Link>
+              <div className="flex justify-center mb-4">
+              <Link href={data.projectUrl} target="_blank">
+              <div onMouseOver={() => 
+                setShowVisitSite(true)} onMouseLeave={() => setShowVisitSite(false)} className="border border-[#818589] rounded-md overflow-hidden w-[325px] sm:w-[500px] cursor-pointer  transition duration-200 ease-in-out ease-in-out hover:opacity-[.7] " >
+                    {isMedium ? <Image src={`https:${data.imageUrl.fields.file.url}`} width={500} height={250} alt="pog digital image"/> : <Image src={`https:${data.imageUrl.fields.file.url}`} width={325} height={125} alt="pog digital image"/>}
+              
+              <AnimatePresence>
+                {showVisitSite &&  <motion.div
+              className="text-center"
+                initial={{ opacity: 0, y: '0px' }}
+                animate={{ opacity: 1, y: '-50px' }}
+                transition={{ type: "spring", duration: 1.25}}
+                exit={{y: '0px', opacity: 0}}
+              ><div className='absolute text-[#6082B6] bg-white  box-shadow opacity-[.2] h-[70px] font-[600] w-[100%]'>
+        
+              </div>
+              <div className="absolute text-[19px] text-[#6082B6] mt-3 font-[600] w-[100%] transition duration-200 ease-in-out ease-in-out hover:text-[#03a9f4]">Visit Website</div>
+            
+              </motion.div>}
+             
+              </AnimatePresence>
+             
+              </div>
+              </Link>
+              </div>
+              <div>
+              { data.summaryDescription && <div className="mt-2 w-[100%]">{data.summaryDescription }</div>}
+              </div>
+            
+<div className="mt-2 w-[100%] sm:px-2 md:px-6">
+{data.description.map((d: any) => <p className="pt-1" key={d}>{`• ${d}`}</p>)}
+              </div>
+              </motion.div>
+    )
+}
+
+
+export default ProjectComponent
