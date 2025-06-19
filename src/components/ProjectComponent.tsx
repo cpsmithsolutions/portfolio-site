fdf746f9feb10f765a0c091ed13a3e850920f325
clarkpsmith <clark.smith79@gmail.com>
2025-06-19 16:43:10 -0600

    image modal and tooltip

--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -6,8 +6,11 @@ import { motion, AnimatePresence } from "framer-motion"
 import { useMediaQuery } from 'react-responsive'
 
 const ProjectComponent = ({ data }: { data: any }) => {
-  const [showVisitSite, setShowVisitSite] = useState<boolean>(false)
   const [isMedium, setIsMedium] = useState(true)
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
+  const [showHoverPopup, setShowHoverPopup] = useState<boolean>(false) // Hover popup state
+
+  console.log({ showHoverPopup })
 
   const isMediumSize = useMediaQuery({
     query: '(min-width: 768px)',
@@ -17,6 +20,13 @@ const ProjectComponent = ({ data }: { data: any }) => {
     setIsMedium(isMediumSize)
   }, [isMediumSize])
 
+  const handleImageClick = () => {
+    setIsModalOpen(true) // Open the modal when the image is clicked
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false) // Close the modal
+  }
   // comment
 
   return (
@@ -46,106 +56,36 @@ const ProjectComponent = ({ data }: { data: any }) => {
       </div>
       <div className="flex flex-col w-[100%] ml:flex-row">
         <div className="flex justify-center mb-4">
-          {data.projectUrl ? (
-            <Link href={data.projectUrl} target="_blank">
-              <div
-                onMouseOver={() => setShowVisitSite(true)}
-                onMouseLeave={() => setShowVisitSite(false)}
-                className="border border-[#818589] rounded-md overflow-hidden w-[325px] md:w-[400px] cursor-pointer  transition duration-200 ease-in-out ease-in-out hover:opacity-[.7] "
-              >
-                {isMedium ? (
-                  <Image
-                    src={`https:${data.imageUrl.fields.file.url}`}
-                    width={400}
-                    height={250}
-                    alt="pog digital image"
-                  />
-                ) : (
-                  <Image
-                    src={`https:${data.imageUrl.fields.file.url}`}
-                    width={325}
-                    height={125}
-                    alt="pog digital image"
-                  />
-                )}
-
-                <AnimatePresence>
-                  {showVisitSite && (
-                    <motion.div
-                      className="text-center"
-                      initial={{ opacity: 0, y: '0px' }}
-                      animate={{ opacity: 1, y: '-50px' }}
-                      transition={{ type: 'spring', duration: 1.25 }}
-                      exit={{ y: '0px', opacity: 0 }}
-                    >
-                      <div className="absolute text-[#6082B6] bg-white  box-shadow opacity-[.8] h-[70px] font-[600] w-[100%]"></div>
-                      <div className="absolute text-[19px] text-[#6082B6] mt-3 font-[600] w-[100%] transition duration-200 ease-in-out ease-in-out hover:text-[#03a9f4]">
-                        Visit Website
-                      </div>
-                    </motion.div>
-                  )}
-                </AnimatePresence>
+          <div
+            className="border border-[#818589] rounded-md overflow-hidden w-[325px] md:w-[400px] cursor-pointer  transition duration-200 ease-in-out ease-in-out hover:opacity-[.7] cursor-pointer"
+            onClick={handleImageClick}
+            onMouseEnter={() => setShowHoverPopup(true)} // Show popup on hover
+            onMouseLeave={() => setShowHoverPopup(false)} // Hide popup when hover ends
+          >
+            {isMedium ? (
+              <Image
+                src={`https:${data.imageUrl.fields.file.url}`}
+                width={400}
+                height={250}
+                alt="project image"
+              />
+            ) : (
+              <Image
+                src={`https:${data.imageUrl.fields.file.url}`}
+                width={325}
+                height={125}
+                alt="project image"
+              />
+            )}
+            {showHoverPopup && (
+              // <div className="absolute -mt-8 top-0 left-0 w-[325px] z-10 md:w-[400px] flex justify-center items-center">
+              <div className="relative">
+                <div className="absolute w-full -mt-6 text-white text-sm font-bold flex justify-center">
+                  <p>Click to View Full-Screen</p>
+                </div>
               </div>
-            </Link>
-          ) : (
-            <div className="border border-[#818589] rounded-md overflow-hidden w-[325px] md:w-[400px]">
-              {isMedium ? (
-                <Image
-                  src={`https:${data.imageUrl.fields.file.url}`}
-                  width={400}
-                  height={250}
-                  alt=" project image"
-                />
-              ) : (
-                <Image
-                  src={`https:${data.imageUrl.fields.file.url}`}
-                  width={325}
-                  height={125}
-                  alt="project image"
-                />
-              )}
-            </div>
-          )}
-          {/* <Link href={data.projectUrl} target="_blank">
-            <div
-              onMouseOver={() => setShowVisitSite(true)}
-              onMouseLeave={() => setShowVisitSite(false)}
-              className="border border-[#818589] rounded-md overflow-hidden w-[325px] md:w-[400px] cursor-pointer  transition duration-200 ease-in-out ease-in-out hover:opacity-[.7] "
-            >
-              {isMedium ? (
-                <Image
-                  src={`https:${data.imageUrl.fields.file.url}`}
-                  width={400}
-                  height={250}
-                  alt="pog digital image"
-                />
-              ) : (
-                <Image
-                  src={`https:${data.imageUrl.fields.file.url}`}
-                  width={325}
-                  height={125}
-                  alt="pog digital image"
-                />
-              )}
-
-              <AnimatePresence>
-                {showVisitSite && (
-                  <motion.div
-                    className="text-center"
-                    initial={{ opacity: 0, y: '0px' }}
-                    animate={{ opacity: 1, y: '-50px' }}
-                    transition={{ type: 'spring', duration: 1.25 }}
-                    exit={{ y: '0px', opacity: 0 }}
-                  >
-                    <div className="absolute text-[#6082B6] bg-white  box-shadow opacity-[.8] h-[70px] font-[600] w-[100%]"></div>
-                    <div className="absolute text-[19px] text-[#6082B6] mt-3 font-[600] w-[100%] transition duration-200 ease-in-out ease-in-out hover:text-[#03a9f4]">
-                      Visit Website
-                    </div>
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </div>
-          </Link> */}
+            )}
+          </div>
         </div>
         <div className="flex flex-col text-[14px] lg:text-[16px] sm:pl-4 xl:pl-6">
           <div>
@@ -160,6 +100,50 @@ const ProjectComponent = ({ data }: { data: any }) => {
           </div>
         </div>
       </div>
+
+      {/* Full-Screen Modal */}
+      <AnimatePresence>
+        {isModalOpen && (
+          <motion.div
+            className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center z-50"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="relative">
+              <button
+                className="absolute top-4 right-4 text-white text-2xl"
+                onClick={handleCloseModal} // Close modal on button click
+              >
+                ✕
+              </button>
+              <Image
+                src={`https:${data.imageUrl.fields.file.url}`}
+                width={1200}
+                height={800}
+                alt="Expanded project image"
+                className="rounded-md"
+              />
+              {/* Visit Site Link or Fallback Text */}
+              <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+                {data.projectUrl ? (
+                  <Link
+                    href={data.projectUrl}
+                    target="_blank"
+                    className="text-white text-lg font-bold bg-[#6082B6] px-4 py-2 rounded-md hover:bg-[#506a9b] transition duration-300"
+                  >
+                    Visit Site
+                  </Link>
+                ) : (
+                  <p className="text-white text-lg font-bold">
+                    Site No Longer Available
+                  </p>
+                )}
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </motion.div>
   )
 }
